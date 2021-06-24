b75420709d7e3896faab396472512fa8de1eb10b
Vitaly Grinberg <vgrinber@redhat.com>
2021-06-24 20:21:42 +0300

    Do uniform logging

--- polgen-pod/src/get-pre-sync-rv.sh
+++ polgen-pod/src/get-pre-sync-rv.sh
@@ -5,9 +5,9 @@
 CM=$(oc get configmap/rv &> /dev/null)
 if (( $? == 0 )); then
   RV=$( oc get configmap/rv -ojson | jq -rM '.metadata.resourceVersion' )
-  echo "$(date -R) RAN post-sync [INFO] Retrieved RAN sites resourceVersion $RV" >> /proc/1/fd/2
+  echo "ztp-site-generator.postsync $(date -R) INFO [post-sync-entrypoint] Retrieved RAN sites resourceVersion $RV" >> /proc/1/fd/2
   echo $RV
 else
-  echo "$(date -R) RAN post-sync [ERROR] Failed to get RAN sites resourceVersion" >> /proc/1/fd/2
+  echo "ztp-site-generator.postsync $(date -R) ERROR [post-sync-entrypoint] Failed to get RAN sites resourceVersion" >> /proc/1/fd/2
   echo "0"
 fi
--- polgen-pod/src/pre-sync-entrypoint.sh
+++ polgen-pod/src/pre-sync-entrypoint.sh
@@ -7,12 +7,19 @@ if oc get configmap/rv &> /dev/null; then
     oc delete configmap/rv &> /dev/null
 fi
 
-#RV=$(curl -s $APISERVER/apis/ran.openshift.io/v1alpha1/namespaces/$NAMESPACE/sites --header "Authorization: Bearer $TOKEN" --cacert $CACERT | jq -rM '.metadata.resourceVersion')
-RV=$(curl -s $APISERVER/apis/ran.openshift.io/v1/siteconfigs --header "Authorization: Bearer $TOKEN" --cacert $CACERT | jq -rM '.metadata.resourceVersion')
+RESP=$(curl -s -w "%{http_code}" $APISERVER/apis/ran.openshift.io/v1/siteconfigs --header "Authorization: Bearer $TOKEN" --cacert $CACERT)
+RC=$(echo $RESP | python -c "print(input()[-3:])")
+RV=$(echo $RESP | python -c "print(input()[:-3])" | jq -rM '.metadata.resourceVersion')
 
-# Store in configmap
-if oc create configmap rv --from-literal=sitesResourceVersion=$RV; then
-    # Log even if ran manually during debugging
-    echo "$(date -R) RAN pre-sync [INFO] Recording RAN sites resourceVersion = $RV" >> /proc/1/fd/1
+if [ $RC != "200" ];then
+    echo "ztp-site-generator.presync $(date -R) ERROR [pre-sync-entrypoint] $APISERVER/apis/ran.openshift.io/v1/siteconfigs call returned $RC" >> /proc/1/fd/1
+    exit 1
+else
+    # Store in configmap
+    if oc create configmap rv --from-literal=sitesResourceVersion=$RV; then
+        # Log even if ran manually during debugging
+        echo "ztp-site-generator.presync $(date -R) INFO [pre-sync-entrypoint] Recording RAN sites resourceVersion = $RV" >> /proc/1/fd/1
+    else
+        echo "ztp-site-generator.presync $(date -R) ERROR [pre-sync-entrypoint] Config map creation failed" >> /proc/1/fd/1
+    fi
 fi
-
